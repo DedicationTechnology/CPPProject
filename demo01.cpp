c334790937c13d8f5d3c4c1e473ce18f4550f0b6
DedicationTechnology <2358241108@qq.com>
2022-04-03 21:50:13 +0800

    添加项目文件。

---
+++ demo01.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>  // printf依赖
+#include<stdlib.h>  // system依赖
+#define _CRT_SECURE_NO_WARNINGS     //这个宏定义最好要放到文件的第一行,在运行的时候不会出现警告提示
+int main000() {
+	//system("pause");  // pause程序的作用是等待用户输入任意键
+	extern int test;  // extern用于告诉编译器有这个变量但是没有定义所以无法赋值
+	//test = 10;  // >>> 会报错
+	const int test01 = 10;  // const 修饰的变量不能被更改
+	//test01 = 100;  // const 修饰的变量不能通过变量名去修改变量的内容
+	printf("%d", test01);
+	return 0;
+}
---
+++ demo02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+namespace name01 {
+	int test01 = 10;
+}
+
+
+namespace name02 {
+	int test01 = 100;
+}
+
+
+int main() {
+	cout << name02::test01 << endl;  // 空间名::变量名 可以访问指定空间内的内容,防止无法同名
+}
